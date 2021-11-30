a2b25f07e87fda6b35846ba06d2d4fd8cc6971dd
alejandro <ftvalex@hotmail.com>
2021-11-30 08:35:24 +0000

    Botones materialUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Button from '@mui/material/Button'
+import SendIcon from '@mui/icons-material/Send'
+import Stack from '@mui/material/Stack';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      funciono
+     <Stack spacing={2} direction="row">
+      <Button variant="contained" color="success"  >
+      Hello World
+      </Button>
+      <Button variant="outlined" size="small" >
+      Hello World
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+      Send
+      </Button>
+      <Button variant="contained" fullWidth>
+      Soy el full
+      </Button>
+      </Stack>
     </div>
   );
 }
